26cf5c51489de36748a7328610364f561a0be9c5
WEbkunx <stormvanya@gmail.com>
2019-12-06 02:25:02 +0200

    added customers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [ProductsModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [ProductsModule, TypeOrmModule.forRoot(typeOrmConfig), CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
